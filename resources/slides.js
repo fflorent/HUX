7a0b90b1c0a214907e36b8a0281f8b8e959e6807
Florent FAYOLLE <fayolle-florent@orange.fr>
2012-05-12 20:31:50 +0200

    add new feature doc + removed jukebow demo

--- resources/slides.js
+++ resources/slides.js
@@ -4,6 +4,9 @@ HUX.Slide = {
 		HUX.Compat.Event.addEventListener(document, "keydown", this.treatSlideChange);
 		var slideCont = this.getSlideContainer();
 	},
+	getSlidePath: function(num){
+		return "contents/demos/slides/"+num+".html";
+	},
 	nbSlides: 7,
 	curSlide: 0,
 	getSlideContainer: function(){
@@ -13,18 +16,17 @@ HUX.Slide = {
 		var self = HUX.Slide, elDir;
 		if( self.getSlideContainer() !== null ){
 			if(event.keyCode === 39 && self.curSlide < self.nbSlides-1){ // NEXT
-				HUX.AtInclusion.addAt("slide", "s_"+(++self.curSlide));
+				HUX.AtInclusion.addAt("slide", self.getSlidePath( ++self.curSlide ));
 			}
 			else if(event.keyCode === 37 && self.curSlide > 0){ // PREVIOUS
 				self.curSlide--;
 				if(self.curSlide >= 1)
-					HUX.AtInclusion.addAt("slide", "s_"+self.curSlide);
+					HUX.AtInclusion.addAt("slide", self.getSlidePath( self.curSlide ));
 				else
 					HUX.AtInclusion.removeAt("slide");
 			}
 			
 		}
 	}
-	
 };
 HUX.addModule(HUX.Slide);
